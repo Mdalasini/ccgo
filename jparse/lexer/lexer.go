f3929cfc1a7f122df15a9088378a043c49a87573
Mdalasini <pathsofgrassymonkey@gmail.com>
2026-04-16 22:57:40 +0300

    Add escape sequence support to string lexer

--- jparse/lexer/lexer.go
+++ jparse/lexer/lexer.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"strconv"
 	"strings"
 )
 
@@ -40,14 +41,51 @@ func (l *Lexer) consumeString() (string, error) {
 			}
 			return "", err
 		}
-		if peek[0] == '"' {
+		switch peek[0] {
+		case '"':
 			l.reader.ReadByte()
-			break
+			return value.String(), nil
+		case '\\':
+			l.reader.ReadByte()
+			peek, err := l.reader.Peek(1)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					return "", fmt.Errorf("escaped character at end of string")
+				}
+				return "", err
+			}
+			switch peek[0] {
+			case 'u':
+				l.reader.ReadByte()
+				peek, err := l.reader.Peek(4)
+				if err != nil {
+					if errors.Is(err, io.EOF) {
+						return "", fmt.Errorf("expected 4 hex digits after \\u")
+					}
+					return "", err
+				}
+
+				// Parse the 4 hex digits as a Unicode code point
+				n, err := strconv.ParseUint(string(peek), 16, 32)
+				if err != nil {
+					return "", fmt.Errorf("invalid hex digit: %s", string(peek))
+				}
+				value.WriteRune(rune(n))
+
+				for range 4 {
+					l.reader.ReadByte()
+				}
+			case '"', '\\', '/', 'b', 'f', 'n', 'r', 't':
+				value.WriteString(string(peek[0]))
+				l.reader.ReadByte()
+			default:
+				return "", fmt.Errorf("invalid escape character: %c", peek[0])
+			}
+		default:
+			value.WriteString(string(peek[0]))
+			l.reader.ReadByte()
 		}
-		value.WriteString(string(peek[0]))
-		l.reader.ReadByte()
 	}
-	return value.String(), nil
 }
 
 func Tokenize(reader *bufio.Reader) ([]Token, error) {
@@ -86,3 +124,5 @@ func Tokenize(reader *bufio.Reader) ([]Token, error) {
 	tokens = append(tokens, newToken(EOF, ""))
 	return tokens, nil
 }
+
+func parseUnicodeEscape()
