710ef6c7f73adb094a774350ecc8daab8d489d3f
Mdalasini <pathsofgrassymonkey@gmail.com>
2026-04-17 22:27:47 +0300

    Replace CLI parsing with simple stub
    The main.go file has been simplified to just print a hello message,
    removing file argument handling, tokenization, and parsing logic.

--- jparse/main.go
+++ jparse/main.go
@@ -1,46 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"flag"
 	"fmt"
-	"os"
-
-	"github.com/mdalasini/ccgo/jparse/lexer"
-	"github.com/mdalasini/ccgo/jparse/parser"
 )
 
-func usage() {
-	fmt.Println("Usage: jparse <file>")
-	flag.PrintDefaults()
-}
-
 func main() {
-	flag.Usage = usage
-	flag.Parse()
-	args := flag.Args()
-
-	if len(args) == 0 {
-		flag.Usage()
-		return
-	}
-
-	file, err := os.Open(args[0])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "jparse: %v", err)
-		os.Exit(1)
-	}
-	defer file.Close()
-
-	reader := bufio.NewReader(file)
-	tokens, err := lexer.Tokenize(reader)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-
-	if err := parser.Parse(tokens); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+	fmt.Println("Hello from jparse!")
 }
