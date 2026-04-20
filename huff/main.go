f971116350692716bc0b190fbe4eea956220bd75
Mdalasini <pathsofgrassymonkey@gmail.com>
2026-04-20 22:55:05 +0300

    Add character frequency counter from file input

--- huff/main.go
+++ huff/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func countCharFrequencies(file *os.File) map[rune]int {
+	frequencies := make(map[rune]int)
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanRunes)
+	for scanner.Scan() {
+		char := scanner.Text()
+		frequencies[rune(char[0])]++
+	}
+
+	return frequencies
+}
 
 func main() {
-	fmt.Println("hello from huff")
+	filepath := "tests/test.txt"
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatalf("unable to open %v: %v", filepath, err)
+	}
+	defer file.Close()
+
+	countCharFrequencies(file)
 }
